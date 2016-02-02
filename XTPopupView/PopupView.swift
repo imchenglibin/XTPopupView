a1f9f4de872b78ac927c656ce332b41e4ce9995d
小天 <2248247504@qq.com>
2016-02-02 23:22:24 +0800

    remove no use code

--- XTPopupView/PopupView.swift
+++ XTPopupView/PopupView.swift
@@ -9,10 +9,6 @@
 import UIKit
 import SnapKit
 
-public enum PopupViewCustomViewPosition {
-    case Center, Bottom, Custom
-}
-
 public class PopupView: UIView {
     private var _tapGesture: UITapGestureRecognizer?
     
@@ -70,26 +66,5 @@ public class PopupView: UIView {
     
     public func animationWhenHide(complete:()->Void) {
         complete()
-    }
-    
-    public func setCustomView(customView: UIView, position: PopupViewCustomViewPosition) {
-//        self.customView = customView
-//        _customViewPositin = position
-//        _size = CGSize(width: customView.frame.width, height: customView.frame.size.height)
-//        addSubview(customView)
-//        if position == .Bottom {
-//            customView.snp_makeConstraints { (make) -> Void in
-//                make.bottom.equalTo(self)
-//                make.left.equalTo(self)
-//                make.right.equalTo(self)
-//                make.height.equalTo(customView.frame.size.height)
-//            }
-//        } else if position == .Center {
-//            customView.snp_makeConstraints { (make) -> Void in
-//                make.center.equalTo(self)
-//                make.width.equalTo(customView.frame.size.width)
-//                make.height.equalTo(customView.frame.size.height)
-//            }
-//        }
     }
 }
